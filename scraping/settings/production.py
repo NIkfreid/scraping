b56e2dcc0c15c81ef756a94254c41ea3b6b15fce
niksonnn <nik.nik.nosov.1985@mail.ru>
2020-07-31 21:30:36 +0400

    change settings

--- scraping/settings/production.py
+++ scraping/settings/production.py
@@ -35,7 +35,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["scrap-serv.herokuapp.com/"]
+ALLOWED_HOSTS = ["scrap-serv.herokuapp.com/", "localhost"]
 
 # Application definition
 
